a0337a37040c5bb8fa791b5ed663fbed3d0f9694
Wisnu Wijokangko <wisnuwijo33@gmail.com>
2023-11-05 13:14:38 +0700

    upgrade node version to 20

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T1Pv20D0yqengi9SvjgA0vycfhIuCCUAT9R9cDrKnkk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"afS4ba+1yeljhw7zn12uE2aJKJEdx9Z/6DBn85QinOQ=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55bc05c1276bb9a8"; }
+/******/ 		__webpack_require__.h = function() { return "6268ec6dce7dc1b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
