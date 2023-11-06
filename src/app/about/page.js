de3310424cef1f34a022b4717aced91be8339e72
Wisnu Wijokangko <wisnuwijo33@gmail.com>
2023-11-06 23:45:12 +0700

    add about page

---
+++ src/app/about/page.js
@@ -0,0 +1,80 @@
+import { InboxArrowDownIcon, LockClosedIcon } from '@heroicons/react/20/solid'
+
+export default function About() {
+    return (
+        <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+            <div className="absolute inset-0 -z-10 overflow-hidden">
+                <svg
+                    className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+                            width={200}
+                            height={200}
+                            x="50%"
+                            y={-1}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <path d="M100 200V.5M.5 .5H200" fill="none" />
+                        </pattern>
+                    </defs>
+                    <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                        <path
+                            d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+                            strokeWidth={0}
+                        />
+                    </svg>
+                    <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+                </svg>
+            </div>
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+                <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                    <div className="lg:pr-4">
+                        <div className="lg:max-w-lg">
+                            <p className="text-base font-semibold leading-7 text-indigo-600">About</p>
+                            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Wisnu Wijokangko</h1>
+                            {/* <p className="mt-6 text-xl leading-8 text-gray-700"></p> */}
+                        </div>
+                    </div>
+                </div>
+                <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                    <div className="rounded-full overflow-hidden">
+                        <img
+                            className="rounded-xl ring-1 ring-gray-400/10"
+                            src="https://dygdaya.com/wisnu02.png"
+                            alt=""
+                        />
+                    </div>
+                </div>
+                <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                    <div className="lg:pr-4">
+                        <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+                            <p>
+                                Passionate software engineer with a specialization in backend development. With years of experience in the tech industry, I've had the privilege of leading talented teams of engineers, guiding them towards success. My journey in this field has been driven by an insatiable curiosity and a constant desire to learn new things. 
+                                Hailing from the charming city of Kudus in Central Java ( 🇮🇩), I bring a unique perspective to my work. I am dedicated to helping companies and teams progress, whether it's through optimizing code, building robust systems, or fostering a culture of continuous improvement.
+                            </p>
+                            <ul role="list" className="mt-8 space-y-8 text-gray-600">
+                                <li className="flex gap-x-3">
+                                    <InboxArrowDownIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-900">Email</strong> <br/>
+                                        dti.wisnu@gmail.com
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-900">Phone Number</strong> <br/>
+                                        +62-882-3203-2477
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
